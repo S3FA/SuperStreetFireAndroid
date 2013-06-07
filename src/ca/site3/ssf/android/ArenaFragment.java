645267dde8a12951794a4b45dfd8a77a0dd36b83
katee <hello@kate.io>
2013-06-06 21:09:44 -0400

    Adds back consumer for fire effect changes.

--- src/ca/site3/ssf/android/ArenaFragment.java
+++ src/ca/site3/ssf/android/ArenaFragment.java
@@ -38,22 +38,22 @@ public class ArenaFragment extends Fragment {
 	// Battery status - is this in the IOServer yet?
 	// information messages from server to tablet
 	// remove player moves, add ringmaster moves and sound effects
-	
-	String[] playersName = {"Player 1", "Player 2"};
-	
+
+	String[] playersName = { "Player 1", "Player 2" };
+
 	RingView ringView;
 	ColorPallet colorPallet;
 	GameControlsView gameControlsView;
 	GameRoundsView roundsView;
-	
+
 	TextView roundTimer;
 	TextView gameInfo;
 	TextView[] playerName;
 	TextView[] playerLastMove;
-	
-	FireEmitterChangedTask task;
-	
+
 	GamemasterMovesControlsView gamemasterControls;
+
+	FireEmitterChangedTask fireEventConsumer;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -62,16 +62,19 @@ public class ArenaFragment extends Fragment {
 
 		ringView = (RingView) view.findViewById(R.id.ring_view);
 		ringView.invalidate();
-		
+
 		ringView.onEmitterTouch = ringView.new OnEmitterTouch() {
 			@Override
-			public void onEmitterTouch(RingView.Emitter emitter, FireEmitter.Location location, int index) {
+			public void onEmitterTouch(RingView.Emitter emitter,
+					FireEmitter.Location location, int index) {
 				// FIXME make this an intent
 				Intent flameTouch = new Intent(Intents.EVENT_TOUCH_FIRE_EMITTER);
 				EnumSet<Entity> contributors = EnumSet.noneOf(Entity.class);
 				contributors.add(Entity.PLAYER1_ENTITY);
-				flameTouch.putExtra(Intents.EXTRA_TOUCH_CONTRIBUTORS, contributors);
-				flameTouch.putExtra(Intents.EXTRA_FIRE_EMITTER_LOCATION, location);
+				flameTouch.putExtra(Intents.EXTRA_TOUCH_CONTRIBUTORS,
+						contributors);
+				flameTouch.putExtra(Intents.EXTRA_FIRE_EMITTER_LOCATION,
+						location);
 				flameTouch.putExtra(Intents.EXTRA_FIRE_EMITTER_INDEX, index);
 				getActivity().sendBroadcast(flameTouch);
 			}
@@ -100,38 +103,41 @@ public class ArenaFragment extends Fragment {
 				if (intentString == Intents.INIT_NEXT_1) {
 					if (gameControlsView.nextStates.size() > 0) {
 						intent = new Intent(Intents.INIT_NEXT);
-						intent.putExtra(Intents.EXTRA_STATE, gameControlsView.nextStates.get(0));
+						intent.putExtra(Intents.EXTRA_STATE,
+								gameControlsView.nextStates.get(0));
 					}
-				}
-				else if (intentString == Intents.INIT_NEXT_2) {
+				} else if (intentString == Intents.INIT_NEXT_2) {
 					if (gameControlsView.nextStates.size() > 1) {
 						intent = new Intent(Intents.INIT_NEXT);
-						intent.putExtra(Intents.EXTRA_STATE, gameControlsView.nextStates.get(1));
+						intent.putExtra(Intents.EXTRA_STATE,
+								gameControlsView.nextStates.get(1));
 					}
 				}
 				getActivity().sendBroadcast(intent);
 			}
 		};
-		
-		roundTimer = (TextView)view.findViewById(R.id.round_timer);
-		
-		gameInfo = (TextView)view.findViewById(R.id.game_info);
-		
+
+		roundTimer = (TextView) view.findViewById(R.id.round_timer);
+
+		gameInfo = (TextView) view.findViewById(R.id.game_info);
+
 		playerLastMove = new TextView[2];
-		playerLastMove[0] = (TextView)view.findViewById(R.id.player_1_info);
-		playerLastMove[1] = (TextView)view.findViewById(R.id.player_2_info);
-		
+		playerLastMove[0] = (TextView) view.findViewById(R.id.player_1_info);
+		playerLastMove[1] = (TextView) view.findViewById(R.id.player_2_info);
+
 		playerName = new TextView[2];
-		playerName[0] = (TextView)view.findViewById(R.id.player_1_name);
-		playerName[1] = (TextView)view.findViewById(R.id.player_2_name);
-		
+		playerName[0] = (TextView) view.findViewById(R.id.player_1_name);
+		playerName[1] = (TextView) view.findViewById(R.id.player_2_name);
+
 		playerName[0].setText(playersName[0]);
 		playerName[1].setText(playersName[1]);
-		
-		gamemasterControls = (GamemasterMovesControlsView)view.findViewById(R.id.gamemaster_moves);
+
+		gamemasterControls = (GamemasterMovesControlsView) view
+				.findViewById(R.id.gamemaster_moves);
 		gamemasterControls.onPlayerActionSelect = gamemasterControls.new OnPlayerActionSelect() {
 			@Override
-			public void onAction(ActionFactory.ActionType action, boolean leftHand, boolean rightHand) {
+			public void onAction(ActionFactory.ActionType action,
+					boolean leftHand, boolean rightHand) {
 				Intent intent = new Intent(Intents.EVENT_GAMEMASTER_MOVE);
 				intent.putExtra(Intents.EXTRA_ACTION_TYPE, action);
 				intent.putExtra(Intents.EXTRA_LEFT_HAND, leftHand);
@@ -139,72 +145,81 @@ public class ArenaFragment extends Fragment {
 				getActivity().sendBroadcast(intent);
 			}
 		};
-		
+
 		roundsView = (GameRoundsView) view.findViewById(R.id.rounds);
-		
-		// Controls will be enabled after we connect if we are in an appropriate game state
+
+		// Controls will be enabled after we connect if we are in an appropriate
+		// game state
 		this.gamemasterControls.setEnabled(false);
 		this.colorPallet.setEnabled(false);
-		
-		task = new FireEmitterChangedTask();
-		task.execute();
 
 		return view;
 	}
-	
-	public void handleGameModelEvent(final IGameModelEvent event) {
-		switch (event.getType()) {
-			case PLAYER_HEALTH_CHANGED:
-				PlayerHealthChangedEvent newEvent = (PlayerHealthChangedEvent)event;
-				ringView.playerHealth[newEvent.getPlayerNum() - 1] = newEvent.getNewLifePercentage();
-				ringView.invalidate();
-				break;
-			case ROUND_PLAY_TIMER_CHANGED:
-				RoundPlayTimerChangedEvent roundEvent = (RoundPlayTimerChangedEvent)event;
-				updateTimer(roundEvent.getTimeInSecs());
-				break;
-			case GAME_INFO_REFRESH:
-				GameInfoRefreshEvent refreshEvent = (GameInfoRefreshEvent)event;
-				if (refreshEvent.getRoundInPlayTimer() > 0) {
-					roundTimer.setText("" + refreshEvent.getRoundInPlayTimer());
-				} else {
-					roundTimer.setText("");
-				}
-				ringView.playerHealth[0] = refreshEvent.getPlayer1Health();
-				ringView.playerHealth[1] = refreshEvent.getPlayer2Health();
-				handleStateChange(refreshEvent.getCurrentGameState());
-				updateTimer(refreshEvent.getRoundInPlayTimer());
-				ringView.postInvalidate();
-				break;
-			case PLAYER_BLOCK_ACTION:
-				PlayerBlockActionEvent blockAction = (PlayerBlockActionEvent)event;
-				playerLastMove[blockAction.getPlayerNum() - 1].setText(String.format("BLOCK"));
-				break;
-			case PLAYER_ATTACK_ACTION:
-				PlayerAttackActionEvent attackAction = (PlayerAttackActionEvent)event;
-				playerLastMove[attackAction.getPlayerNum() - 1].setText(attackAction.getAttackType().toString());
-				break;
-			case GAME_STATE_CHANGED:
-				GameStateChangedEvent stateChanged = (GameStateChangedEvent)event;
-				handleStateChange(stateChanged.getNewState());
-				break;
-			case ROUND_ENDED:
-				RoundEndedEvent roundEndedEvent = (RoundEndedEvent)event;
-				roundsView.handleRoundEndedEvent(roundEndedEvent);
-				break;
-			case MATCH_ENDED:
-				// reset names
-				playerName[0].setText(playersName[0]);
-				playerName[1].setText(playersName[1]);
-				// clear last moves
-				playerLastMove[0].setText("");
-				playerLastMove[1].setText("");
-				break;
-			default:
-				break;
-		}				
+
+	@Override
+	public void onResume() {
+		fireEventConsumer = new FireEmitterChangedTask();
+		fireEventConsumer.execute();
+		super.onResume();
 	}
-	
+
+	public void handleGameModelEvent(final IGameModelEvent event) {
+		Log.i(LOG_TAG, event.toString());
+		switch (event.getType()) {
+		case PLAYER_HEALTH_CHANGED:
+			PlayerHealthChangedEvent newEvent = (PlayerHealthChangedEvent) event;
+			ringView.playerHealth[newEvent.getPlayerNum() - 1] = newEvent
+					.getNewLifePercentage();
+			ringView.invalidate();
+			break;
+		case ROUND_PLAY_TIMER_CHANGED:
+			RoundPlayTimerChangedEvent roundEvent = (RoundPlayTimerChangedEvent) event;
+			updateTimer(roundEvent.getTimeInSecs());
+			break;
+		case GAME_INFO_REFRESH:
+			GameInfoRefreshEvent refreshEvent = (GameInfoRefreshEvent) event;
+			if (refreshEvent.getRoundInPlayTimer() > 0) {
+				roundTimer.setText("" + refreshEvent.getRoundInPlayTimer());
+			} else {
+				roundTimer.setText("");
+			}
+			ringView.playerHealth[0] = refreshEvent.getPlayer1Health();
+			ringView.playerHealth[1] = refreshEvent.getPlayer2Health();
+			handleStateChange(refreshEvent.getCurrentGameState());
+			updateTimer(refreshEvent.getRoundInPlayTimer());
+			ringView.postInvalidate();
+			break;
+		case PLAYER_BLOCK_ACTION:
+			PlayerBlockActionEvent blockAction = (PlayerBlockActionEvent) event;
+			playerLastMove[blockAction.getPlayerNum() - 1].setText(String
+					.format("BLOCK"));
+			break;
+		case PLAYER_ATTACK_ACTION:
+			PlayerAttackActionEvent attackAction = (PlayerAttackActionEvent) event;
+			playerLastMove[attackAction.getPlayerNum() - 1]
+					.setText(attackAction.getAttackType().toString());
+			break;
+		case GAME_STATE_CHANGED:
+			GameStateChangedEvent stateChanged = (GameStateChangedEvent) event;
+			handleStateChange(stateChanged.getNewState());
+			break;
+		case ROUND_ENDED:
+			RoundEndedEvent roundEndedEvent = (RoundEndedEvent) event;
+			roundsView.handleRoundEndedEvent(roundEndedEvent);
+			break;
+		case MATCH_ENDED:
+			// reset names
+			playerName[0].setText(playersName[0]);
+			playerName[1].setText(playersName[1]);
+			// clear last moves
+			playerLastMove[0].setText("");
+			playerLastMove[1].setText("");
+			break;
+		default:
+			break;
+		}
+	}
+
 	public void updateTimer(int time) {
 		if (time > 0) {
 			roundTimer.setText("" + time);
@@ -212,46 +227,46 @@ public class ArenaFragment extends Fragment {
 			roundTimer.setText("");
 		}
 	}
-	
+
 	public void handleStateChange(GameState.GameStateType stateType) {
 		gameControlsView.handleStateChange(stateType);
 		switch (stateType) {
-			case ROUND_IN_PLAY_STATE:
-			case TIE_BREAKER_ROUND_STATE:
-			case TEST_ROUND_STATE:
-				this.setEnableActionControls(true, false);
-				break;
-				
-			case RINGMASTER_STATE:
-				this.setEnableActionControls(false, true);
-				break;
-				
-			default:
-				this.setEnableActionControls(false, false);
-				break;
+		case ROUND_IN_PLAY_STATE:
+		case TIE_BREAKER_ROUND_STATE:
+		case TEST_ROUND_STATE:
+			this.setEnableActionControls(true, false);
+			break;
+
+		case RINGMASTER_STATE:
+			this.setEnableActionControls(false, true);
+			break;
+
+		default:
+			this.setEnableActionControls(false, false);
+			break;
 		}
 	}
-	
-    private class FireEmitterChangedTask extends AsyncTask<Void, Void, Void> {
+
+	private void setEnableActionControls(boolean enabledPlayerControls,
+			boolean enabledRingmasterControls) {
+		this.gamemasterControls.setEnabled(enabledRingmasterControls);
+		this.colorPallet.setEnabled(enabledRingmasterControls);
+	}
+
+	public class FireEmitterChangedTask extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected Void doInBackground(Void... arg0) {
-			Log.e(LOG_TAG, "starting consumer");
-			BlockingQueue<FireEmitterChangedEvent> q = ((SSFApplication)getActivity().getApplication()).getInstance().fireEvents;
+			BlockingQueue<FireEmitterChangedEvent> q = ((SSFApplication) getActivity()
+					.getApplication()).getInstance().fireEvents;
 			try {
 				while (true) {
-					Log.e(LOG_TAG, "emitter change consumed");
 					ringView.handleFireEmitterEvent(q.take());
 				}
 			} catch (InterruptedException e) {
 				Log.e(LOG_TAG, "consumer died");
-				Log.e(LOG_TAG, e.toString());
+				e.printStackTrace();
 			}
 			return null;
 		}
-    }
-    
-	private void setEnableActionControls(boolean enabledPlayerControls, boolean enabledRingmasterControls) {
-		this.gamemasterControls.setEnabled(enabledRingmasterControls);
-		this.colorPallet.setEnabled(enabledRingmasterControls);
 	}
 }
